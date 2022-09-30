421418d20ebd2b1c10317f2f4eca027c809c8eb9
Czar <71781648+This-is-Czar@users.noreply.github.com>
2022-09-30 19:23:39 +0530

    Create Add.cpp

---
+++ Add.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+//add2 number
+
+int main()
+{
+  int a,b,s;
+   scanf("%d",&a);
+   scanf("%d",&b);
+  s=a+b;
+  printf("%d",s);
+
+
+  return 0;
+}
